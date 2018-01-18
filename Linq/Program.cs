ade1c42f105f16890e5fc6ff2f663da39d7b5195
Wescley Matos <wescleymatos@gmail.com>
2018-01-18 13:59:41 -0300

    Implementação de linq

---
+++ Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lista = {3, 9, 4, 6, 20, 10, 60, 90, 80, 50};
+
+            var listaFiltrada1 = lista.Where(x => x > 10).OrderBy(x => x);
+            var listaFiltrada2 = from a in lista where a < 10 orderby a select a;
+
+            foreach (var item in listaFiltrada1)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in listaFiltrada2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
