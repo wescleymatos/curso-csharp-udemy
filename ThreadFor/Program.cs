ff5b7cb84407484189abeaba18431cedde0deafc
Wescley Matos <wescleymatos@gmail.com>
2018-01-25 15:30:14 -0300

    exemplo de thread

---
+++ ThreadFor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(ThreadRepeticao);
+            t.Start();
+
+            for (int i = 0; i >= 1000 ; i++)
+            {
+                Console.WriteLine("Main: " + i);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao()
+        {
+            for (int i = 0; i >= 1000 ; i++)
+            {
+                Console.WriteLine("ThreadRepeticao: " + i);
+            }
+        }
+    }
+}
